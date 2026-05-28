70f5beeb05c8359057dd29c628a5e413a3671986
DobryiEN <rc74zp5y2n@privaterelay.appleid.com>
2026-05-28 23:04:05 +0300

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+using namespace std;
+
+int robotX = 0;
+int robotY = 0;
+
+void moveUp() {
+    if (robotY > 0) robotY--;
+}
+
+void moveDown() {
+    if (robotY < 9) robotY++;
+}
+
+void moveLeft() {
+    if (robotX > 0) robotX--;
+}
+
+void moveRight() {
+    if (robotX < 9) robotX++;
+}

69c656eb79e17bd9936710d1e10ef7fb6de3dbd7
gulinskii <gulinskij_nd_25@student.grsu.by>
2026-05-29 20:22:47 +0300

    Добавлена карта и система очистки мусора

--- main.cpp
+++ main.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
-#include "functions.h"
+#include <vector>
 
 using namespace std;
+
+const int SIZE = 10;
 
 extern int robotX;
 extern int robotY;
 
-int main() {
+vector<vector<char>> mapGrid(SIZE, vector<char>(SIZE, '.'));
 
-    createTrash();
+void createTrash() {
+    mapGrid[2][3] = 'M';
+    mapGrid[5][5] = 'M';
+    mapGrid[7][1] = 'M';
+}
 
-    string command;
+void drawMap() {
 
-    while (true) {
+    system("cls");
 
-        drawMap();
+    for (int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < SIZE; x++) {
 
-        cout << "===== СИМУЛЯТОР ЭКО-ЛОГИКИ =====" << endl;
-        cout << "Команды: UP DOWN LEFT RIGHT CLEAN EXIT" << endl;
-        cout << "Введите команду: ";
-
-        cin >> command;
-
-        if (command == "UP") moveUp();
-        else if (command == "DOWN") moveDown();
-        else if (command == "LEFT") moveLeft();
-        else if (command == "RIGHT") moveRight();
-        else if (command == "CLEAN") cleanTrash();
-        else if (command == "EXIT") break;
+            if (x == robotX && y == robotY)
+                cout << "R ";
+            else
+                cout << mapGrid[y][x] << " ";
+        }
+        cout << endl;
     }
+}
 
-    return 0;
+void cleanTrash() {
+
+    if (mapGrid[robotY][robotX] == 'M') {
+        mapGrid[robotY][robotX] = '.';
+        cout << "Мусор убран!" << endl;
+    } else {
+        cout << "Здесь нет мусора!" << endl;
+    }
 }

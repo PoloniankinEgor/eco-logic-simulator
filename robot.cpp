f2e353784e3b1a6bead3ca45c43424c2b2b34ca3
PoloniankinEgor <polonynkin_ea_25@student.grsu.by>
2026-06-01 21:56:36 +0300

    Добавлен подсчёт количества перемещений робота во время работы симулятора

--- robot.cpp
+++ robot.cpp
@@ -5,19 +5,40 @@ using namespace std;
 int robotX = 0;
 int robotY = 0;
 int score = 0;
+int moves = 0;
 
 void moveUp() {
-    if (robotY > 0) robotY--;
+
+    if (robotY > 0) {
+
+        robotY--;
+        moves++;
+    }
 }
 
 void moveDown() {
-    if (robotY < 9) robotY++;
+
+    if (robotY < 9) {
+
+        robotY++;
+        moves++;
+    }
 }
 
 void moveLeft() {
-    if (robotX > 0) robotX--;
+
+    if (robotX > 0) {
+
+        robotX--;
+        moves++;
+    }
 }
 
 void moveRight() {
-    if (robotX < 9) robotX++;
+
+    if (robotX < 9) {
+
+        robotX++;
+        moves++;
+    }
 }

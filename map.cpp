dde9c2070b8f45f01cfcd29e6e6ec7789ebb76e1
DomostojSV <Domostoj_SV_25@student.grsu.by>
2026-06-02 22:12:41 +0300

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -18,6 +18,21 @@ void createTrash() {
 
     srand(time(0));
 
+    int created = 0;
+
+    while (created < 3) {
+
+        int x = rand() % SIZE;
+        int y = rand() % SIZE;
+
+        if (mapGrid[y][x] != 'M') {
+
+            mapGrid[y][x] = 'M';
+            created++;
+        }
+    }
+}
+
     for (int i = 0; i < 3; i++) {
 
         int x = rand() % 10;

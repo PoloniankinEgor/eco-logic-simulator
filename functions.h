cf47c5c666f3c8967a19d45eaaf78e5589fcc228
gulinskii <gulinskij_nd_25@student.grsu.by>
2026-06-03 20:11:01 +0300

    Подготовлены объявления функций для автоматического режима работы робота

--- functions.h
+++ functions.h
@@ -3,8 +3,23 @@
 void drawMap();
 void createTrash();
 void cleanTrash();
+
 void moveUp();
 void moveDown();
 void moveLeft();
 void moveRight();
+
+void moveTo(int x, int y);
+
+int getRobotX();
+int getRobotY();
+
+int getRobotEnergy();
+int getRobotScore();
+int getRobotMoves();
+
+void addEnergy(int value);
+void spendEnergy(int value);
+void addScore();
+
 void autoClean();
